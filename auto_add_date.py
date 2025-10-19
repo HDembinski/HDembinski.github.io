2be2b63801270e575083f352dc912d1ca54dbca2
Hans Dembinski <hans.dembinski@gmail.com>
2025-10-19 12:30:12 +0200

    u

--- auto_add_date.py
+++ auto_add_date.py
@@ -1,13 +1,29 @@
 import sys
 import nbformat
 import subprocess as subp
+import argparse
 
-for fn in sys.argv[1:]:
+parser = argparse.ArgumentParser()
+parser.add_argument("--dry-run", action="store_true")
+parser.add_argument("file", nargs="+")
+
+args = parser.parse_args()
+
+for fn in args.file:
     with open(fn) as f:
         nb = nbformat.read(f, as_version=4)
 
     r = subp.run(
-        ["git", "log", "--follow", "--format=%ad", "--date=short", fn], stdout=subp.PIPE
+        [
+            "git",
+            "log",
+            "--find-renames=90%",
+            "--follow",
+            "--format=%ad",
+            "--date=short",
+            fn,
+        ],
+        stdout=subp.PIPE,
     )
     dates = [x for x in r.stdout.decode().split("\n") if x]
     date = dates[-1]
@@ -22,5 +38,6 @@ date: {date}
 
         nb.cells = [nbformat.v4.new_raw_cell(front_matter)] + nb.cells
 
-    with open(fn, "w") as f:
-        nbformat.write(nb, f)
+    if not args.dry_run:
+        with open(fn, "w") as f:
+            nbformat.write(nb, f)
