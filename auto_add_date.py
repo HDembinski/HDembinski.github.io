a1b81e31f68b64338af024aa49eafbc4b2fb0431
Hans Dembinski <hans.dembinski@gmail.com>
2025-10-19 14:45:52 +0200

    u

--- auto_add_date.py
+++ auto_add_date.py
@@ -1,8 +1,6 @@
-import sys
 import nbformat
 import subprocess as subp
 from pathlib import Path
-import os
 
 
 def get_date(fn: Path):
@@ -22,23 +20,28 @@ def get_date(fn: Path):
     return dates[-1]
 
 
-output_path = Path("posts_preprocessed")
+output_path = Path("posts")
+output_path.mkdir(exist_ok=True)
 
-input_files = [Path(fn) for fn in Path("posts").rglob("*.*")]
+input_files = [Path(fn) for fn in Path("_posts").rglob("*.*")]
 
 for fn in input_files:
     if fn.suffix not in (".ipynb", ".md"):
         continue
 
+    fn_out = output_path / fn.name
     date = get_date(fn)
-
     front_matter = f"""---
 date: {date}
 ---
 """
-
     with open(fn, encoding="utf-8") as f:
-        doc = nbformat.read(f, as_version=4)
-        if doc.cells and doc.cells[0].cell_type != "raw":
-            doc.cells = [nbformat.v4.new_raw_cell(front_matter)] + doc.cells
-        nbformat.write(doc, output_path / fn.name)
+        if fn.suffix == ".ipynb":
+            doc = nbformat.read(f, as_version=4)
+            if doc.cells and doc.cells[0].cell_type != "raw":
+                doc.cells = [nbformat.v4.new_raw_cell(front_matter)] + doc.cells
+            nbformat.write(doc, fn_out)
+        elif fn.suffix == ".md":
+            content = f.read()
+            with open(fn_out, "w", encoding="utf-8") as f:
+                f.write(front_matter + content)
